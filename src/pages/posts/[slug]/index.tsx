a5c3f2e69a01c5455cfc23616bc8837dd44b8699
rockerrishabh <rishabh.6542@gmail.com>
2022-07-15 02:38:08 +0530

    sixteenth

--- src/pages/posts/[slug]/index.tsx
+++ src/pages/posts/[slug]/index.tsx
@@ -40,7 +40,7 @@ function Post() {
     try {
       publishPost.mutateAsync({ id });
       toast.success("Successfully Published");
-      router.reload();
+      router.prefetch(`/posts/${post.data?.id}`);
     } catch (error) {
       toast.error("Error while Publishing...");
     }
@@ -57,7 +57,7 @@ function Post() {
     try {
       unPublishPost.mutateAsync({ id });
       toast.success("Successfully Un-Published");
-      router.reload();
+      router.prefetch(`/posts/${post.data?.id}`);
     } catch (error) {
       toast.error("Error while Un-Publishing...");
     }
