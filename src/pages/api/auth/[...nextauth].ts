154b23a529e64937bf1a8313677bc3f053a9cb52
rockerrishabh <rishabh.6542@gmail.com>
2022-07-19 17:24:12 +0530

    twenty-five

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,10 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
-
+// NextAuth options
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
@@ -21,22 +20,10 @@ export const authOptions: NextAuthOptions = {
         },
       },
     }),
+
     // ...add more providers here
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "Enter your Email Address",
-        },
-      },
-      async authorize(credentials, _req) {
-        const user = { id: 1, email: credentials?.email ?? "J Smith" };
-        return user;
-      },
-    }),
   ],
+
   callbacks: {
     redirect: async (params: { url: string; baseUrl: string }) =>
       Promise.resolve(params.url),
