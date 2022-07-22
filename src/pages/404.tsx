6367ba571991e87b358ef62d48e91171600f40f6
rockerrishabh <rishabh.6542@gmail.com>
2022-07-22 19:43:30 +0530

    thirty-one

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,11 +2,7 @@ import ErrorPage from "../components/Error";
 import Layout from "../components/Layout";
 
 function NotFoundPage() {
-  return (
-    <Layout>
-      <ErrorPage pageError="Not Found" errorTitle="Not Found" />
-    </Layout>
-  );
+  return <ErrorPage pageError="Not Found" errorTitle="Not Found" />;
 }
 
 export default NotFoundPage;
